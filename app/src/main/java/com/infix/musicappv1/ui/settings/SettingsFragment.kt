0d4514d4a751804ad9cf32768fe4c1b57dc07290
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-02-27 16:17:24 +0700

    create strings/arrays xml variant for vi

--- app/src/main/java/com/infix/musicappv1/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/infix/musicappv1/ui/settings/SettingsFragment.kt
@@ -2,6 +2,7 @@ package com.infix.musicappv1.ui.settings
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import com.infix.musicappv1.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
