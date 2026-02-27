04decf3f444796bc5413040c8fc998ca9180a953
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-02-27 14:55:57 +0700

    setup settings fragment

--- app/src/main/java/com/infix/musicappv1/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/infix/musicappv1/ui/settings/SettingsFragment.kt
@@ -2,7 +2,6 @@ package com.infix.musicappv1.ui.settings
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
-import com.infix.musicappv1.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
