70442fc9ea447c24dc605f58f8134d1afa171e20
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-08-05 01:05:19 +0700

    Update Song class fields and increment MusicDatabase version to 11

--- app/src/main/java/com/infix/musicappv1/data/source/local/db/MusicDatabase.kt
+++ app/src/main/java/com/infix/musicappv1/data/source/local/db/MusicDatabase.kt
@@ -46,7 +46,7 @@ import com.infix.musicappv1.data.source.local.user.UserDAO
         SearchKeySong::class,
         RecentSearchSong::class
     ],
-    version = 10,
+    version = 11,
     // autoMigrations = [AutoMigration(from = 3, to = 4)]
 )
 @TypeConverters(value = [DateConverter::class])
